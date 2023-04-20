1bcc5bc320ac51b62ace4fc60a96cde4186715bd
Bishwajit Behera <bishwajit@revidd.com>
2023-04-20 15:10:59 +0530

    Added navigation bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
@@ -23,11 +24,7 @@ export default function Home() {
 
   return (
     <>
-      <h1 className="text-4xl text-red-700">Netflix Clone</h1>
-      <p className="text-white">Logged in as :{user?.email}</p>
-      <button onClick={() => signOut()} className="h-10 w-full bg-white">
-        Logout
-      </button>
+      <Navbar />
     </>
   );
 }
